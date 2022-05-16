2d9fc23aa8fc62d65659014ce39e1e09d2c9a616
Ege <93480309+egeozgur@users.noreply.github.com>
2022-05-16 16:37:25 +0300

    Add files via upload

---
+++ NotUygulamam/app/src/main/java/not/uygulamam/notuygulamam/MainActivity.java
@@ -0,0 +1,16 @@
+package not.uygulamam.notuygulamam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static veritaba
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
